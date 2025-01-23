11e88c4488b98db2cde24a6eb45b062c0dad1c03
LeDai-FS <152073694+quangBig@users.noreply.github.com>
2025-01-23 23:15:57 +0700

    API login

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -34,7 +34,6 @@ export class AuthGuard implements CanActivate {
     }
     private extractTokenFromHeader(request: Request) {
         console.log("request:", request);
-
         const [type, token] = request.headers.authorization?.split(' ') ?? [];
         return type === 'Bearer' ? token : undefined;
     }
